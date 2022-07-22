fa3ac6bf3fd546807a6fd74b5a8766f968db5999
Tin <chenti11@oocl.com>
2022-07-22 14:19:38 +0800

    feat: return null given full parking lot and a car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -13,6 +13,10 @@ public class ParkingLot {
     }
 
     public Ticket park(Car car) {
+        if(capacity == 0){
+            return null;
+        }
+        capacity --;
         return new Ticket();
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -3,12 +3,13 @@ package com.parkinglot;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ParkingLotTest {
 
 
     @Test
-    void should_return_a_parking_when_park_given_a_parking_lot_and_a_car() {
+    void should_return_a_ticket_when_park_given_a_parking_lot_and_a_car() {
         //given
         ParkingLot parkingLot = new ParkingLot(2);
 
@@ -19,5 +20,17 @@ public class ParkingLotTest {
         assertNotNull(ticket);
     }
 
-    
+    @Test
+    void should_return_null_when_park_given_a_full_parking_lot_and_a_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot(1);
+
+        //when
+        Ticket ticketFist = parkingLot.park(new Car());
+        Ticket ticketSecond = parkingLot.park(new Car());
+
+        //then
+        assertNotNull(ticketFist);
+        assertNull(ticketSecond);
+    }
 }
