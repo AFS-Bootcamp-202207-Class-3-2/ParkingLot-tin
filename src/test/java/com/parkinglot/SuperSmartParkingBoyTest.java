7778f89321fd9bd1026cbfb5714ac8499c126581
chenqitian <369077336@qq.com>
2022-07-23 11:37:22 +0800

    feat: park when a smart parkink boy with second parking lot has more rate

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -3,15 +3,17 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SuperSmartParkingBoyTest {
     //Story 6
     @Test
-    void should_park_first_lot_when_park_given_with_two_parking_lots_of_the_same_rate_and_a_car() {
+    void should_park_first_lot_when_park_given_with_two_parking_lots_of_the_same_rate() {
         //given
         List<ParkingLot> parkingLotList = new ArrayList<>();
         parkingLotList.add(new ParkingLot(3));
@@ -23,5 +25,24 @@ public class SuperSmartParkingBoyTest {
         Ticket ticket = smartParkingBoy.park(new Car());
         //then
         assertTrue(parkingLotList.get(0).hasContainsKey(ticket));
+    }
+
+    @Test
+    void should_park_second_lot_when_park_given_with_second_parking_lot_more_rate() {
+        //given
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(new ParkingLot(3));
+        parkingLotList.add(new ParkingLot(3));
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotList);
+        smartParkingBoy.park(new Car());
+
+        //when
+        assertEquals(parkingLotList.get(1).calculateVacancyRate(),parkingLotList.stream()
+                .map(ParkingLot::calculateVacancyRate)
+                .max(Double::compareTo)
+                .get());
+        Ticket ticket = smartParkingBoy.park(new Car());
+        //then
+        assertTrue(parkingLotList.get(1).hasContainsKey(ticket));
     }
 }
